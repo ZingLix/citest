5b572d25560fbdd1da0d8f9d5d1825e488c7fb98
root <liujun45@baidu.com>
2023-10-10 14:15:09 +0800

    example upload

---
+++ citest/hello.py
@@ -0,0 +1,5 @@
+def add(a: int, b: int) -> int:
+    """
+    example function
+    """
+    return a + b
---
+++ citest/tests/add_test.py
@@ -0,0 +1,6 @@
+import hello
+import pytest 
+
+def test_add():
+    assert hello.add(1,2) == 3
+    assert hello.add(1,3) == 4
